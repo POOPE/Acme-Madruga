6f35604958667c6d160adec50d36eaf887007ece
pope <menamajeff@gmail.com>
2019-03-07 01:41:46 +0100

    Auto stash before rebase of "origin/sprint"

--- src/main/java/domain/EnrollmentLog.java
+++ src/main/java/domain/EnrollmentLog.java
@@ -3,6 +3,9 @@ package domain;
 
 import java.util.Date;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -11,7 +14,9 @@ import javax.validation.constraints.Pattern;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-public class EnrollmentLog {
+@Entity
+@Access(AccessType.PROPERTY)
+public class EnrollmentLog extends DomainEntity {
 
 	private Member				member;
 	private Brotherhood			brotherhood;
